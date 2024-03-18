71abcf8482905a21e1294dd81d9567231de48a5a
Abhishek Sharma <avisheksharma2004@gmail.com>
2024-03-19 02:10:17 +0530

    Video done

--- public/script.js
+++ public/script.js
@@ -1,19 +1,21 @@
+const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement('video');
 myVideo.muted = true;
-
 
 let myVideoStream 
 navigator.mediaDevices.getUserMedia({
     video: false,
-    audio: true
+    audio: true,
 }).then(stream => {
     myVideoStream = stream;
     addvideoStream(myVideo , stream);
 })
 
-const addvideoStream = (video , stram)=> {
-    video.src.Object = stream;
-    video.addEventListner('loadmetadata' , ()=> {
+const addvideoStream = (video , stream)=> {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata' , ()=> {
         video.play();
     } )
+
+    videoGrid.append(video);
 }
